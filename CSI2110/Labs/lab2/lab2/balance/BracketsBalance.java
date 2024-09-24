6f95144684d78e37f65b730b0e6980ebc80c9c29
Zuhair Zubair <zzuba024@uottawa.ca>
2024-09-24 14:09:46 -0400

    added

--- CSI2110/Labs/lab2/lab2/balance/BracketsBalance.java
+++ CSI2110/Labs/lab2/lab2/balance/BracketsBalance.java
@@ -30,6 +30,21 @@ class BracketsBalance {
     private boolean isBalanced(String exp) {
 
         // INSERT YOUR CODE HERE 
+		final String opening = "({["; // opening delimiters
+		final String closing = ")}]"; // respective closing delimiters
+		Stack<Character> buffer = new LinkedStack<Character>( );
+		// above could use another concrete data structure: ArrayStack<Character>()
+		for (char c : expression.toCharArray( )) {
+		if (opening.indexOf(c) != −1) // this is a left delimiter
+			buffer.push(c);
+		else if (closing.indexOf(c) != −1) { // this is a right delimiter
+			if (buffer.isEmpty( )) // nothing to match with
+				return false;
+			if (closing.indexOf(c) != opening.indexOf(buffer.pop( )))
+				return false; // mismatched delimiter
+			}
+		}
+		return buffer.isEmpty( ); // were all opening delimiters matched?
 
         
 
